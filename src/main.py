3b11197bf942cf638b2ff59d0862a21ac652e0d5
darwinalexis793 <darwinalexiscadavid@gmail.com>
2025-10-31 05:17:14 -0500

    un pequeño esquema del sistema de rol y mejora en la estructura del proyecto

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Header, Request, Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.exceptions import HTTPException
 from jose import jwt
@@ -22,7 +22,7 @@ def decode_token(token: Annotated[str, Depends(oauth2_scheme)]) -> dict:
     user = users.get(data["username"])
     return user
 
-@app.post("/token")
+@app.post("/token", tags=['login'])
 def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = users.get(form_data.username)
     if not user or form_data.password != user["password"]:
@@ -30,6 +30,23 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     token = encode_token({"username": user["username"], "email": user["email"]})
     return {"access_token": token}
 
-@app.get("/users/profile")
+@app.get("/users/profile", tags=['login'])
 def profile(my_user: Annotated[dict, Depends(decode_token)]):
     return my_user
+
+
+#ACCESS AND ROLE
+
+
+
+@app.get("/dashboard", tags=['access and role'])
+def deshboart(
+    request: Request,
+    reponse: Response,
+    access_token: Annotated[str | None, Header()] = None,
+    user_role: Annotated[str | None, Header()] = None,
+    ):
+    if access_token != "secret-token":
+        raise HTTPException(status_code=401, detail="no autorizado")
+    reponse.headers["user_status"] = "enabled"
+    return {"access_token": access_token, "user_role": user_role}
